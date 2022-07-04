e2bba10cac60ba2dc79035ae3ad5b543ff02f3f0
Geekay13 <95465923+Geekay13@users.noreply.github.com>
2022-07-04 11:33:50 +0530

    Initial Commit

---
+++ JavaAbstractClass.java
@@ -0,0 +1,19 @@
+public class JavaAbstractClass
+{
+   
+   public static void main(String []args)
+   {
+      int year =1701;
+      if (year % 400==0) {
+    	  System.out.println("leap year");
+      }
+      else if(year % 4==0){
+    	  System.out.println("leap year");
+      }
+     
+      else {
+    	  System.out.println("not leap year");
+      }
+      
+   }
+}
